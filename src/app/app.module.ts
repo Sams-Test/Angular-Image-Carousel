6dac3c3533372e9a64feabdbee5bb99b83d6d873
Sam David <samd948@gmail.com>
2018-03-24 20:26:19 +0530

    imported img-carousel selector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ImgCarouselModule } from '../img-carousel/img-carousel.module';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ImgCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
